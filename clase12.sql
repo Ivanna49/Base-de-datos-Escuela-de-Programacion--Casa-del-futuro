04f63b50b5563f4264f7985b124275fff1feb2c8
Ivanna López Barbona <109434696+Ivanna49@users.noreply.github.com>
2024-04-25 13:56:48 -0300

    Clase 12
    Clase 12 -GROUP BY, HAVING

---
+++ clase12.sql
@@ -0,0 +1,44 @@
+#¿Cuántas películas hay?
+SELECT COUNT(*) 
+FROM movies;
+
+#¿Cuántas películas tienen entre 3 y 7 premios?
+SELECT COUNT(*)
+FROM movies
+WHERE awards BETWEEN 3 AND 7;
+
+#¿Cuántas películas tienen entre 3 y 7 premios y un rating mayor a 7?
+SELECT COUNT(*)
+FROM movies
+WHERE awards BETWEEN 3 AND 7
+AND rating > 7;
+
+#Encuentra la cantidad de actores en cada película.
+SELECT movie_id, COUNT(actor_id) AS cantidad_actores
+FROM actor_movie
+GROUP BY movie_id;
+
+#Crear un listado a partir de la tabla de películas, mostrar un reporte de la cantidad de películas por id. de género
+SELECT genre_id, COUNT(*) AS cantidad_peliculas
+FROM movies
+GROUP BY genre_id;
+
+#De la consulta anterior, listar sólo aquellos géneros que tengan como suma de premios un número mayor a 5.
+SELECT genre_id, COUNT(*) AS cantidad_peliculas
+FROM movies
+GROUP BY genre_id
+HAVING SUM(awards) > 5;
+
+
+#Encuentra los géneros que tienen las películas con un promedio de calificación mayor a 6.0.
+SELECT name, AVG(ranking) AS promedio_calificacion
+FROM genres
+GROUP BY name
+HAVING AVG(ranking) > 6.0;
+
+#Encuentra los géneros que tienen al menos 3 películas.
+SELECT genre_id, COUNT(*) AS cantidad_peliculas
+FROM movies
+GROUP BY genre_id
+HAVING COUNT(*) >= 3;
+
